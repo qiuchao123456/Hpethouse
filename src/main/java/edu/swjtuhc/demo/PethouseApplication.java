5e04883828792e24b9bae9c7a9359e19299ab312
qiuchao123456 <910007298@qq.com>
2019-12-07 10:22:05 +0800

    上传的项目

---
+++ src/main/java/edu/swjtuhc/demo/PethouseApplication.java
@@ -0,0 +1,13 @@
+package edu.swjtuhc.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PethouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PethouseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/swjtuhc/demo/PethouseApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.swjtuhc.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PethouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
